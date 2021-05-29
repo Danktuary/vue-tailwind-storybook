a41d9251809d1fa03c95c9274f76cd7734b25ae6
Sanctuary <sanc.pw@gmail.com>
2021-05-29 14:12:56 -0400

    feat: Install and setup Tailwind

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,3 +1,5 @@
+const path = require('path');
+
 module.exports = {
 	stories: [
 		'../src/**/*.stories.mdx',
@@ -6,5 +8,19 @@ module.exports = {
 	addons: [
 		'@storybook/addon-links',
 		'@storybook/addon-essentials',
+		'@storybook/addon-postcss',
 	],
+	webpackFinal: async (config) => {
+		config.module.rules.push({
+			test: /\.css$/,
+			use: [
+				{
+					loader: 'postcss-loader',
+				},
+			],
+			include: path.resolve(__dirname, '../src'),
+		});
+
+		return config;
+	},
 };
